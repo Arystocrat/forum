7ea9b91599cbf386caf537b6f5f7b1e41e25d315
Arystocrat <ArystocratV@gmail.com>
2025-11-08 19:39:06 +0500

    добавил страницы но их делал гпт

--- public/index.php
+++ public/index.php
@@ -1,23 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>METANIT.COM</title>
-    <meta charset="utf-8" />
-</head>
-<body>
-<table>
-    <?php
-    require_once __DIR__ . '/../src/Router.php';
+<?php
+require_once __DIR__ . '/../src/Router.php';
 
-    $uri = $_SERVER['REQUEST_URI'];
+$uri = $_SERVER['REQUEST_URI'];
 
-    $router = new Router;
+$router = new Router;
 
-    $router->add('/', __DIR__ . '/../src/home.php');
-    $router->add('/about', __DIR__ . '/../src/about.php');
+$router->add('/', __DIR__ . '/../src/home.php');
+$router->add('/about', __DIR__ . '/../src/about.php');
 
-    $router->dispatch($uri);
-    ?>
-</table>
-</body>
-</html>
+$router->dispatch($uri);
--- src/pages/home.php
+++ src/pages/home.php
@@ -1,1 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Главная страница</title>
+</head>
+<body>
+<h1>Добро пожаловать на наш форум!</h1>
+<p>Это главная страница.</p>
+<nav>
+    <a href="/">Главная</a> | <a href="/about">О нас</a>
+</nav>
+</body>
+</html><?php
