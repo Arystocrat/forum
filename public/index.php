807a4878cf0f3f7aa57bb16229611e9ea2023073
Arystocrat <ArystocratV@gmail.com>
2025-11-08 21:50:38 +0500

    изменена архитектура

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
-require_once __DIR__ . '/../src/Router.php';
+require_once __DIR__ . '/../core/Router.php';
 
 $uri = $_SERVER['REQUEST_URI'];
 
 $router = new Router;
 
-$router->add('/', __DIR__ . '/../src/home.php');
-$router->add('/about', __DIR__ . '/../src/about.php');
+$router->add('/', __DIR__ . '/../app/Controllers/home.php');
+$router->add('/about', __DIR__ . '/../app/Controllers/about.php');
 
 $router->dispatch($uri);
